f83634fa83c05d4a2008ffd9fa3275715dba70d6
Víctor Araya A. <129128148+varayaIII@users.noreply.github.com>
2025-07-22 18:39:26 -0400

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,8 @@
+// utils/helpers.js
 
+function formatDate(dateStr) {
+  const date = new Date(dateStr);
+  return date.toLocaleDateString("es-CL", { year: 'numeric', month: 'long', day: 'numeric' });
+}
+
+export { formatDate };
