a273c72c40c15d6854e9a0ff636a7c418442c39d
Víctor Araya A. <129128148+varayaIII@users.noreply.github.com>
2025-07-22 18:34:51 -0400

    Update data.js

--- models/data.js
+++ models/data.js
@@ -1,1 +1,13 @@
+// models/data.js
 
+const projects = [
+  { title: "Proyecto 1", description: "Descripción del proyecto 1", link: "#" },
+  { title: "Proyecto 2", description: "Descripción del proyecto 2", link: "#" }
+];
+
+const blogPosts = [
+  { title: "Post del Blog 1", content: "Contenido del blog 1", date: "2025-07-22" },
+  { title: "Post del Blog 2", content: "Contenido del blog 2", date: "2025-07-21" }
+];
+
+export { projects, blogPosts };
