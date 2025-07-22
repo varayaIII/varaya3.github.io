f134f7c2261c61223649f58f9894fce2bd6a63f8
Víctor Araya A. <129128148+varayaIII@users.noreply.github.com>
2025-07-22 18:35:15 -0400

    Update mainController.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,1 +1,12 @@
+// controllers/mainController.js
+import { projects, blogPosts } from "../models/data.js";
 
+function loadProjects() {
+  return projects;
+}
+
+function loadBlogPosts() {
+  return blogPosts;
+}
+
+export { loadProjects, loadBlogPosts };
